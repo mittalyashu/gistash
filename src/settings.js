6b848e05b78016aa0c8c0aa6920bf8a6b5d3bb12
Yashu Mittal <mittalyashu77@gmail.com>
2020-07-25 11:07:12 +0530

    Split getSettings & saveSettings functions

--- src/settings.js
+++ src/settings.js
@@ -3,24 +3,34 @@ const fs = require("fs");
 const { configFilePath } = require("./globals");
 const interactiveMode = require("./interactiveMode")
 
+let settings = {}
+
 const getSettings = async (configLocation = configFilePath) => {
-  let settings = {}
   try {
     if (fs.existsSync(configLocation)) {
       settings = await fs.readFileSync(configLocation, 'utf8');
       return JSON.parse(settings);
     } else {
-      settings = {
-        ...(await interactiveMode())
-      }
-      await fs.writeFileSync(configFilePath, JSON.stringify(settings))
-      return settings
+      return await saveSettings();
     }
   } catch (error) {
     console.log(error);
   }
 }
 
+const saveSettings = async (configLocation = configFilePath) => {
+  try {
+    settings = {
+      ...(await interactiveMode())
+    }
+    await fs.writeFileSync(configLocation, JSON.stringify(settings))
+    return settings
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
-  get: getSettings
+  get: getSettings,
+  save: saveSettings
 }
